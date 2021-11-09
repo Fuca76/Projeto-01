84dc5f0dbd6d8bcfd0874d52f45c70b4f4d93c0b
Taís Carvalho <taisocarvalhocampos@gmail.com>
2021-11-08 22:40:18 -0300

    vamos

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-alert('Olá ,fico feliz com a sua presença')
+alert('Olá ,fico feliz com a sua presença !')
 var nome = window.prompt('Qual o seu nome?')
 alert(`${nome} , tenho um versículo para a sua meditação \u{1F64C}`)
